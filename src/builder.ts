742e106a71c912f302d68ddf595b12bca7d92ce3
Monica Perez Martin <mperez@lucidchart.com>
2021-07-14 14:04:47 -0600

    Split builder functionality to its own file

---
+++ src/builder.ts
@@ -0,0 +1,127 @@
+'use strict';
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {writeFile, findBuild, findComponents, findScalaFiles} from './file';
+
+enum BuildFileType {
+    ng_project = "ng_project",
+    scala_library = "scala_library",
+    ts_project = "ts_project"
+}
+
+
+function getBazelTemplate(folderPath: string, buildType: string) {
+    if(buildType == BuildFileType.scala_library) {
+        return `load("@rules_scala_annex//rules:scala_with_scalafmt.bzl", "scala_library")
+
+scala_library(
+    name = "${path.basename(folderPath)}",
+    srcs = glob(["*.scala"]),
+    format = True,
+    visibility = ["//visibility:public"],
+    deps = [
+    ],
+)
+    `
+    }
+    
+    return `load("//cake/build/bazel:${buildType}.bzl", "${buildType}")
+
+package(default_visibility = ["//visibility:public"])
+
+${buildType}(
+    name = "${path.basename(folderPath)}",
+    module_name = "${getLucidPrefixedPath(folderPath)}",
+    tsconfig = "//cake/app/webroot/ts:tsconfig.111.json",
+    deps=[
+
+    ],
+)
+`;
+}
+
+function getLucidPrefixedPath(path: string): string {
+    const index = path.indexOf('cake/app/webroot/ts');
+    return '@lucid' + path.substring(index + 19);
+}
+
+function createBuildFile(inFolder: string, buildType: string):Promise<any> {
+    const buildPath = path.join(inFolder, "BUILD.bazel");
+    if (fs.existsSync(buildPath)) {
+        return Promise.reject(new Error("BUILD file already exists in " + inFolder));
+    } else {
+        return new Promise((resolve, reject) => {
+            writeFile(buildPath, getBazelTemplate(inFolder, buildType)).then(resolve, reject);
+        }).then(() => {
+            return vscode.workspace.openTextDocument(buildPath).then((textDoc) => {
+                return vscode.window.showTextDocument(textDoc);
+            });
+        });
+    }
+}
+
+export async function getBuildType(inFolder: string): Promise<string> {
+    const isComponent = findComponents(inFolder).then(foundComponent => {
+        if(foundComponent) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if(await isComponent) {
+        return BuildFileType.ng_project;
+    }
+    const isScalaFile = findScalaFiles(inFolder).then(foundScalaFile => {
+        if(foundScalaFile) {
+            return true;
+        } else {
+            return false;
+        }
+    })
+    const buildType = (await isScalaFile) ? BuildFileType.scala_library : BuildFileType.ts_project;
+    return buildType;
+}
+
+export async function goToBuild(inDirectory: string) {
+    findBuild(inDirectory).then(buildPath => {
+        if(buildPath) {
+            return vscode.workspace.openTextDocument(buildPath).then((textDoc) => {
+                return vscode.window.showTextDocument(textDoc);
+            });
+        } else {
+            vscode.window.showErrorMessage('No path found for BUILD file.');
+        }
+    });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    const createBuildFileListener = vscode.commands.registerCommand('ngTemplates.create-build-file', (uri: vscode.Uri) => {
+        if (!uri.fsPath) {
+            vscode.window.showErrorMessage('No folder selected to contain new build file');
+            return;
+        }
+
+        getBuildType(uri.fsPath).then((buildType) => {
+            return createBuildFile(uri.fsPath, buildType);
+        }).catch((err) => {
+            vscode.window.showErrorMessage(err.toString());
+            console.error(err);
+        });
+    });
+    context.subscriptions.push(createBuildFileListener);
+
+    const findBuildListener = vscode.commands.registerCommand('ngTemplates.find-build', (uri: vscode.Uri) => {
+        
+        if (!uri.fsPath) {
+            vscode.window.showErrorMessage('No folder selected to find a BUILD file');
+            return;
+        }
+        goToBuild(path.dirname(uri.fsPath));
+    });
+    context.subscriptions.push(findBuildListener);
+}
+
+export function deactivate() {
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,17 @@
 'use strict';
 import * as vscode from 'vscode';
 import * as angular from './angular';
+import * as builder from './builder';
 import * as unittests from './unittests';
 
 export function activate(context: vscode.ExtensionContext) {
     angular.activate(context);
     unittests.activate(context);
+    builder.activate(context);
 }
 
 export function deactivate() {
     angular.deactivate();
     unittests.deactivate();
+    builder.deactivate();
 }
